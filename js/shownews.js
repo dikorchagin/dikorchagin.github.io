cbec33cef27902c8e7c54d11ec54657f32c5d328
dikorchagin <buregon@gmail.com>
2017-12-14 10:00:17 +0300

    Fixed alignment, menu script

--- js/shownews.js
+++ js/shownews.js
@@ -1,17 +1,14 @@
 $(document).ready(function() {
-    var currentWidth = '360px';
     
     //Т.к. данные генерируются React`ом, то события надо делегировать статичным тегам.
     $('#newsReact').on( 'click', function( event ) {
         if ($(event.target).attr("class") == "newsblock-small__key") {
                 if ($(event.target).nextAll('.newsblock-small__text-full').css('display') == 'none') {
-                    currentWidth = $(event.target).offsetParent().css('max-width');
-                    $(event.target).offsetParent().css('max-width', '100%');            
                     $(event.target).nextAll('.newsblock-small__text-small').css('display', 'none');
                     $(event.target).nextAll('.newsblock-small__text-full').css('display', 'inline-block');
+                    $('html').animate({ scrollTop: $(event.target).offset().top }, 1100);
                     $(event.target).text('-');
                 } else {
-                    $(event.target).offsetParent().css('max-width', currentWidth);            
                     $(event.target).nextAll('.newsblock-small__text-small').css('display', 'inline-block');
                     $(event.target).nextAll('.newsblock-small__text-full').css('display', 'none');
                     $(event.target).text('+');
